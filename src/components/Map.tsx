068fa7ae8df145211fc35f1967e9bbee29acc153
elguapo87 <pglogovac@gmail.com>
2025-05-18 21:41:02 +0200

    Added dynamic data in Map & Pin components

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,33 +1,57 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+"use client";
+
+import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
 import Pin from "./Pin";
 
-type ListDataTypes = {
-    id: number;
-    title: string;
-    img: string;
-    bedroom: number;
-    bathroom: number;
-    price: number;
-    address: string,
-    latitude: number,
-    longitude: number,
-    city: string;
-    property: string;
+const MapContainer = dynamic(() => import("react-leaflet").then((m) => m.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import("react-leaflet").then((m) => m.TileLayer), { ssr: false });
+
+type PostDetailType = {
+  desc?: string;
+  utilities?: string;
+  pet?: string;
+  income?: string;
+  size?: number;
+  school?: number;
+  bus?: number;
+  restaurant?: number;
 };
 
-const Map = ({ items } : { items: ListDataTypes[] }) => {
+type ImageType = {
+  id: number;
+  url: string;
+};
+
+type PostDataType = {
+  id: number;
+  title: string;
+  images: ImageType[];
+  bedroom: number;
+  bathroom: number;
+  price: number;
+  property: string;
+  address: string;
+  latitude: number;
+  longitude: number;
+  city: string;
+  postDetail: PostDetailType[];
+};
+
+const Map = ({ items }: { items: PostDataType[]}) => {
   return (
-    <MapContainer center={[52.4797, -1.90269]} zoom={7} scrollWheelZoom={false} className="map">
+    <MapContainer center={[44.2107675, 20.9224158]} zoom={7} scrollWheelZoom={true} className="h-[500px] md:h-[83vh] w-full rounded-[20px]">
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {items.map((item) => (
-        <Pin key={item.id} item={item} />
-      ))}
+      {items.map((item) => {
+        return <Pin key={item.id} item={item} />;
+      })}
     </MapContainer>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
+
+
--- src/components/Pin.tsx
+++ src/components/Pin.tsx
@@ -1,35 +1,66 @@
+"use client";
+
+import Image from "next/image";
 import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import Link from "next/link";
 
-type ListDataTypes = {
+const customIcon = new L.Icon({
+    iconUrl: "/marker-icon.png", // Use a local image in `public/` folder
+    shadowUrl: "/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+});
+
+type PostDetailType = {
+    desc?: string;
+    utilities?: string;
+    pet?: string;
+    income?: string;
+    size?: number;
+    school?: number;
+    bus?: number;
+    restaurant?: number;
+  };
+  
+  type ImageType = {
+    id: number;
+    url: string;
+  };
+  
+  type PostDataType = {
     id: number;
     title: string;
-    img: string;
+    images: ImageType[];
     bedroom: number;
     bathroom: number;
     price: number;
-    address: string,
-    latitude: number,
-    longitude: number,
-    city: string;
     property: string;
+    address: string;
+    latitude: number;
+    longitude: number;
+    city: string;
+    postDetail: PostDetailType[];
+  };  
+
+const Pin = ({ item } : { item: PostDataType }) => {
+
+    return (
+        <Marker position={[item.latitude, item.longitude]} icon={customIcon}>
+            <Popup>
+                <Link href={`/${item.id}`} className="w-40 flex gap-4 items-center">
+                  
+                    <Image src={item.images?.[0]?.url} alt="" width={64} height={64} className="w-16 h-16 object-cover rounded-[5px]" />
+               
+                    <div className="flex flex-col justify-between text-sm"> 
+                        <span className="whitespace-nowrap">{item.bedroom === 1 ? item.bedroom + " bedroom" : item.bedroom + " bedrooms"} </span>
+                        <span className="font-semibold">€{item.price}</span>
+                    </div>
+                </Link>
+            </Popup>
+        </Marker>
+    );
 };
 
-const Pin = ({ item } : { item: ListDataTypes }) => {
-  return (
-    <Marker position={[item.latitude, item.longitude]}>
-      <Popup>
-        <div className="popupContainer">
-          <img src={item.img} alt="" />
-          <div className="textContainer">
-            <Link href={`/${item.id}`}>{item.title}</Link>
-            <span>{item.bedroom} bedroom</span>
-            <b>$ {item.price}</b>
-          </div>
-        </div>
-      </Popup>
-    </Marker>
-  )
-}
-
-export default Pin
+export default Pin;
